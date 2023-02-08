781bd9a20fd625d2c3ccc235b6eb95f3e22e7665
Matt Turi <matt.turi@modalai.com>
2023-02-07 16:21:13 -0800

    eigen dep add

--- install_build_deps.sh
+++ install_build_deps.sh
@@ -15,6 +15,7 @@ voxl-opencv
 libboost-filesystem-dev:arm64
 libboost-thread-dev:arm64
 libboost-date-time-dev:arm64
+voxl-eigen3
 voxl-open-vins
 "
 
@@ -24,6 +25,7 @@ libmodal-pipe
 voxl-opencv
 voxl-boost
 voxl-open-vins
+voxl-eigen3
 "
 
 ## this list is just for tab-completion
