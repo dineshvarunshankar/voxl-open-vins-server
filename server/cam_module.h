458a0e47000b914f17028a1848174723cf7815f6
kyle.tyni <kyle.tyni@modalai.com>
2024-10-22 16:35:32 -0700

    moved cam callback into its own file

---
+++ server/cam_module.h
@@ -0,0 +1,16 @@
+#ifndef CAM_MODULE_H
+#define CAM_MODULE_H
+
+#include <modal_pipe.h>
+#include "common.h"
+
+/**
+ * @brief      creates camera pipe clients and associated callbacks
+ *
+ *             This sets the camera helper callbacks and opens the client pipes
+ */
+int connect_cam_service(std::vector<camera_info> &cam_info_vec);
+
+
+
+#endif // CAM_MODULE_H
